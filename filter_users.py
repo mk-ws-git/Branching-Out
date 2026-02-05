d4806573de7b30699adfe0099ebdb9e3c06e95e0
mk-ws-git <maryksprague@gmail.com>
2026-02-05 14:12:02 +0100

    Added feature to search by age and utf-8 encoding

--- filter_users.py
+++ filter_users.py
@@ -2,7 +2,7 @@ import json
 
 
 def filter_users_by_name(name):
-    with open("users.json", "r") as file:
+    with open("users.json", "r",  encoding="utf-8") as file:
         users = json.load(file)
 
     filtered_users = [user for user in users if user["name"].lower() == name.lower()]
@@ -11,11 +11,27 @@ def filter_users_by_name(name):
         print(user)
 
 
+def filter_by_age(min_age, max_age):
+    with open("users.json", "r",  encoding="utf-8") as file:
+        users = json.load(file)
+
+    filtered_users = [user for user in users if min_age <= user["age"] <= max_age]
+
+    for user in filtered_users:
+        print(user)
+
+
 if __name__ == "__main__":
-    filter_option = input("What would you like to filter by? (Currently, only 'name' is supported): ").strip().lower()
+    filter_option = input("Would you like to filter by name or age: ").strip().lower()
 
     if filter_option == "name":
         name_to_search = input("Enter a name to filter users: ").strip()
         filter_users_by_name(name_to_search)
+
+    elif filter_option == "age":
+        min_age = int(input("Enter minimum age: ").strip())
+        max_age = int(input("Enter maximum age: ").strip())
+        filter_by_age(min_age, max_age)
+
     else:
         print("Filtering by that option is not yet supported.")
